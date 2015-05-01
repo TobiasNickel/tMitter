edd4f14d94a626b441a7ad30264c81b6a04d47a3
TobiasNickel <ross40k@googlemail.com>
2015-05-01 18:27:03 +0800

    creating the method function objects only once on the client live cycle.

--- tmitter.js
+++ tmitter.js
@@ -7,28 +7,26 @@
   * gives an object the ability to handle emittions by defining 4 attributes
   * ._events, .on, .off and .trigger
   */
-function tMitter(object){
-	//container to store the listener/callbacks
-	object._events= {};
+tMitter = (function(){
 	/**
 	 *	method that you will want to write in the documentation of your class/object.
 	 *  together with all the events you trigger by yourself
 	 *@param event {string} name of the event
 	 *@param callback {function} the function to be called when the event is triggered
 	 */
-	object.on= function(event, callback) {
+	function on(event, callback) {
 		event = event.toLowerCase();
 		if (!(event in this._events))
 			this._events[event] = [];
 		if (this._events[event].indexOf(callback) === -1)
 			this._events[event].push(callback);
-	};
+	}
 	/**
 	 *	method to remove an eventlistener or even all.
 	 *@param event {string} name of the event where the call back should be removed
 	 *@param callback {function} that will be removed from the listener
 	 */
-	object.off= function(event, callback) {
+	function off(event, callback) {
 		if (!event) {
 			this._events = {};
 			return;
@@ -38,13 +36,13 @@ function tMitter(object){
 		} else {
 			delete this._events[event][this._events[event].indexOf(callback)];
 		}
-	};
-	/**
+	}
+		/**
 	 *  executing all listener that are registered on the event
 	 *@param event {string} name of the event to be triggered
 	 *@args args {mixed} anything that you want to be passed to the listeners callback
 	 */
-	object.trigger= function(event, args) {
+	function trigger(event, args) {
 		if (!event)
 			return;
 		if (!this._events[event])
@@ -54,4 +52,11 @@ function tMitter(object){
 			this._events[event][i](args);
 		}
 	};
-}
+	return function tMitter(object){
+		//container to store the listener/callbacks
+		object._events= {};
+		object.on= on;
+		object.off= off;
+		object.trigger= trigger;
+	};
+})();
